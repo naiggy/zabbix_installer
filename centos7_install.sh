a041db8beabcbad5e9b06aae535dd5f362b27597
naiggy <51503355+naiggy@users.noreply.github.com>
2020-04-01 21:04:00 +0900

    Update centos7_install.sh

--- centos7_install.sh
+++ centos7_install.sh
@@ -3,17 +3,17 @@
 VER=0
 echo "What version do you want to install?"
 echo "1) 4.0 LTS"
-echo "2) Pre 5.0"
+##echo "2) Pre 5.0"
 
-read -p "Please, select a version: " yn
+read -p "Please, select number 1 : " yn
 
 case "$yn" in
     1) echo "Do you want to install 4.0 LTS?";
      VER=1;
      ;;
-    2) echo "Do you want to install Pre 5.0?";
-     VER=2;
-     ;;
+    ##2) echo "Do you want to install Pre 5.0?";
+    ## VER=2;
+    ## ;;
    *) echo "Abort the installation.";
      exit ;;
 esac
@@ -23,13 +23,19 @@ echo "VER="$VER
 if [ "$VER" == "1" ];then
     rpm -Uvh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-2.el7.noarch.rpm
 elif [ "$VER" == "2" ];then
-    rpm -Uvh https://repo.zabbix.com/zabbix/4.5/rhel/8/x86_64/zabbix-release-4.5-2.el8.noarch.rpm
+    rpm -Uvh https://repo.zabbix.com/zabbix/4.5/rhel/7/x86_64/zabbix-release-4.5-2.el7.noarch.rpm
 else
     echo "Abort the installation."
     exit;
 fi
 
-setenforce 0
+if type setenforce >/dev/null 2>&1; then
+    RET=`getenforce`
+    if [ "${RET}" =! "Disabled" ];then
+        setenforce 0
+    fi
+fi
+
 systemctl stop firewalld
 systemctl disable firewalld
 
@@ -40,12 +46,20 @@ fi
 
 yum clean all
 
-yum install -y zabbix-server-mysql zabbix-web-mysql zabbix-apache-conf zabbix-agent
+if [ "$VER" == "1" ];then
+    yum install -y zabbix-server-mysql zabbix-web-mysql zabbix-agent
+    echo date.timezone = `timedatectl | grep "Time zone" | awk '{print $3}'` >> /etc/php.ini
+elif [ "$VER" == "2" ];then
+    yum install -y centos-release-scl
+    yum install -y zabbix-server-mysql zabbix-agent
+    sed -i -z 's/enabled=0/enabled=1/1' < /etc/yum.repos.d/zabbix.repo /etc/yum.repos.d/zabbix.repo
+    yum install -y zabbix-web-mysql-scl zabbix-apache-conf-scl
+    echo php_value[date.timezone] = `timedatectl | grep "Time zone" | awk '{print $3}'` >> /etc/opt/rh/rh-php72/php-fpm.d/zabbix.conf
+fi
 
 yum install -y mariadb-server.x86_64
 systemctl start mariadb
 
-#!/bin/sh
 
 DB=mysql
 PASSWORD="Parkzabbix"
@@ -56,12 +70,11 @@ create database zabbix character set utf8 collate utf8_bin;
 grant all privileges on zabbix.* to zabbix@localhost identified by '${PASSWORD}';
 
 EOS`
-
 echo "$SQL" | mysql -u root
 
 zcat `find  /usr/share/doc/zabbix-server-mysql* |  grep create.sql.gz` | mysql -uzabbix -p${PASSWORD} zabbix
 
-echo date.timezone = `timedatectl | grep "Time zone" | awk '{print $3}'` >> /etc/php.ini
+
 
 sed -i -e "s/# DBPassword=/DBPassword=${PASSWORD}/" /etc/zabbix/zabbix_server.conf
 
@@ -87,7 +100,9 @@ global \$DB; \n\
 \$IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG; "
 echo -e $TEXT > /etc/zabbix/web/zabbix.conf.php
 chmod 644 /etc/zabbix/web/zabbix.conf.php
-systemctl restart httpd
+
+systemctl restart zabbix-server zabbix-agent httpd
+systemctl enable zabbix-server zabbix-agent httpd
 
 echo "Zabbix Install Completed."
 echo "Product by Park.iggy<naiggy@gmail.com>"
